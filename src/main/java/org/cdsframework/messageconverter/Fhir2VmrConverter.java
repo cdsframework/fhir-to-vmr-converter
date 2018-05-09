88a08eb8bb252f15baee156ce7e0beb0de050a01
Samuel Nicolary <sam@nicolary.org>
2018-05-08 21:07:52 -0700

    initial conversion of fhir via hapi

--- src/main/java/org/cdsframework/messageconverter/Fhir2VmrConverter.java
+++ src/main/java/org/cdsframework/messageconverter/Fhir2VmrConverter.java
@@ -1,9 +1,6 @@
 package org.cdsframework.messageconverter;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.util.Map;
 import org.cdsframework.util.LogUtils;
 import org.opencds.vmr.v1_0.schema.CDSInput;
 
@@ -17,13 +14,8 @@ public class Fhir2VmrConverter {
 
     public CDSInput convert(String payload) throws IOException {
         final String METHODNAME = "convert ";
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> map;
-        map = mapper.readValue(payload, new TypeReference<Map<String, Object>>() {
-        });
-        logger.warn(METHODNAME, "map=", map);
-        CDSInput cdsInput = Fhir2Vmr.getCdsInputFromMap(map);
-        return cdsInput;
+        logger.debug(METHODNAME, "payload=", payload);
+        return Fhir2Vmr.getCdsInputFromFhir(payload);
     }
 
 }
